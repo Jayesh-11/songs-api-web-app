1331056361c10ae563582571375c4d7d480ec563
Jayesh-11 <jayeshvyavahare132000@gmail.com>
2022-09-09 08:48:33 +0530

    added music player

--- src/components/Searched.jsx
+++ src/components/Searched.jsx
@@ -2,26 +2,26 @@ import React from "react";
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
-import "./styles/Searched.css";
+import styled from "styled-components";
 
 function Searched() {
   let params = useParams();
-  // const [endPoint, setEndPoint] = useState("");
   const [container, setContainer] = useState([]);
   const [container2, setContainer2] = useState([]);
   const fetchData = (endPoint) => {
     const options = {
       method: "GET",
-      url: "https://shazam.p.rapidapi.com/search",
+      url: "https://spotify23.p.rapidapi.com/search/",
       params: {
-        term: endPoint,
-        locale: "en-US",
+        q: endPoint,
+        type: "multi",
         offset: "0",
-        limit: "5",
+        limit: "10",
+        numberOfTopResults: "5",
       },
       headers: {
-        "X-RapidAPI-Key": process.env.REACT_APP_SHAZAM_API,
-        "X-RapidAPI-Host": "shazam.p.rapidapi.com",
+        "X-RapidAPI-Key": process.env.REACT_APP_SPOT_API,
+        "X-RapidAPI-Host": "spotify23.p.rapidapi.com",
       },
     };
 
@@ -29,55 +29,127 @@ function Searched() {
       .request(options)
       .then(function (response) {
         const data = response.data;
-        setContainer(data.tracks.hits);
-        setContainer2(data.artists.hits);
+        setContainer(data.tracks.items);
       })
       .catch(function (error) {
         console.error(error);
       });
   };
+  //------------------------------------------------Get track api
+
+  const fetchTrackData = (endPointTrack) => {
+    const options = {
+      method: "GET",
+      url: "https://spotify23.p.rapidapi.com/tracks/",
+      params: { ids: endPointTrack },
+      headers: {
+        "X-RapidAPI-Key": process.env.REACT_APP_SPOT_API,
+        "X-RapidAPI-Host": "spotify23.p.rapidapi.com",
+      },
+    };
+
+    axios
+      .request(options)
+      .then(function (response) {
+        const dataTrack = response.data.tracks[0].preview_url;
+        setContainer2(dataTrack);
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  };
+
+  //------------------------------------------------Get track api
+
   useEffect(() => {
     fetchData(params.searchParam);
   }, [params.searchParam]);
 
   return (
-    <div className="searchedSection">
-      <h2>Songs</h2>
-      <div className="cardContainer">
+    <Div1>
+      <H2>Songs</H2>
+      <AudioPlayer1 src={container2} controls></AudioPlayer1>
+      <Div2>
         {container.map((item) => {
           return (
-            <div className="trackCard">
-              <a href={item.track.url} target="_blank">
-                <img
-                  src={item.track.images.coverart}
-                  className="cardImage"
-                  alt="pic here"
-                />
-              </a>
-              <h4>{item.track.title}</h4>
-            </div>
+            <Div3>
+              <ImgCard
+                src={item.data.albumOfTrack.coverArt.sources[0].url}
+                className="cardImage"
+                alt="pic here"
+                onClick={() => {
+                  fetchTrackData(item.data.id);
+                }}
+              />
+
+              <H4>{item.data.name}</H4>
+            </Div3>
           );
         })}
-      </div>
-      <h2>Artists</h2>
-      <div className="cardContainer">
-        {container2.map((item) => {
-          return (
-            <div className="trackCard">
-              <a href={item.artist.weburl} target="_blank">
-                <img
-                  src={item.artist.avatar}
-                  className="cardImage"
-                  alt="pic here"
-                />
-              </a>
-              <h4>{item.artist.name}</h4>
-            </div>
-          );
-        })}
-      </div>
-    </div>
+      </Div2>
+    </Div1>
   );
 }
 
+const Div1 = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  align-items: center;
+  margin-top: 5vh;
+`;
+
+const Div2 = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
+  gap: 3vh;
+`;
+
+const Div3 = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+const ImgCard = styled.img`
+  height: 20vh;
+  width: 20vh;
+  transition: 0.25s;
+  transition-timing-function: cubic-bezier(0.075, 0.82, 0.165, 1);
+  border-radius: 0.4rem;
+  :hover {
+    transform: scale(1.3);
+    border-radius: 1rem;
+  }
+`;
+
+const H2 = styled.h2`
+  text-align: center;
+  font-family: "Fira Sans";
+`;
+
+const H4 = styled.h4`
+  text-align: center;
+  font-family: "Fira Sans";
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  max-width: 14ch;
+`;
+
+const AudioPlayer1 = styled.audio`
+  ::-webkit-media-controls-panel,
+  video::-webkit-media-controls-panel {
+    background-color: white;
+  }
+  position: fixed;
+  width: 100vw;
+  bottom: 0;
+  padding: 0;
+  background-color: white;
+`;
 export default Searched;
